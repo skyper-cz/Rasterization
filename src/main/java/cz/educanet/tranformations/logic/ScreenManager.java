f1b7e0df30a6ed0a521e3523b3b9d1ccced78ef0
skyper-cz <62872636+skyper-cz@users.noreply.github.com>
2021-02-19 11:26:20 +0100

    my updates

--- src/main/java/cz/educanet/tranformations/logic/ScreenManager.java
+++ src/main/java/cz/educanet/tranformations/logic/ScreenManager.java
@@ -21,11 +21,34 @@ public class ScreenManager {
         return selectedPoints.contains(coordinate);
     }
 
+    private double rovnice(Coordinate x, Coordinate y) {
+        double a = ((double) y.getY() - x.getY()) / (y.getX() - x.getX());
+        double result = (double) x.getY() - (a * x.getX());
+        return result;
+    }
+
+
     public Set<Coordinate> getSelectedPoints() {
         return selectedPoints;
     }
 
     public boolean isFilledIn(Coordinate coordinate) { // TODO: Implement this
+        if(getSelectedPoints().size() < 3) {
+            return false;
+        }
+        Coordinate[] xyArray = new Coordinate[3];
+        int i = 0;
+        for (Coordinate getCoords : getSelectedPoints()) {
+            xyArray[i] = getCoords;
+            i++;
+        }
+        float a =(float)(xyArray[1].getY() - xyArray[0].getY() / xyArray[1].getX() - xyArray[0].getX());
+        float b = xyArray[0].getY() - a * coordinate.getX();
+
+        if (coordinate.getY() <= a * coordinate.getX() + b && ) {
+            return true;
+        }
         return false;
+
     }
 }
